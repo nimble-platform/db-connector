8cf66d26591185c8e274e407fad289c570ede490
evgenhvost@gmail.com <hvostenko1>
2018-06-24 11:50:23 +0300

    creating jar

--- update-jar-branch.sh
+++ update-jar-branch.sh
@@ -1,8 +1,18 @@
 #!/usr/bin/env bash
 
-JAR_NAME="db-connector-0.0.1.jar"
+echo "Switching branch"
+git checkout jar-branch
+
+echo "Building the Jar"
+mvn clean compile assembly:single &> grep "BUILD SUCCESS"
+[[ $? == 0 ]] || ( echo "Failed to build" && exit 1 )
+
+JAR_NAME="db-connector.jar"
 
 echo "Copying the Jar locally"
 cp target/${JAR_NAME} ./
 
+git add -f ${JAR_NAME} && git commit -m "Adding the jar"
 
+echo "Returning to master"
+git checkout master
